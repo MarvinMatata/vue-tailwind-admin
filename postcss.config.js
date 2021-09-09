4d8e112e62b11eea1e491359271f82a76743fd20
MarvinMatata <marvotata@gmail.com>
2021-09-09 12:50:40 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	"plugins": [
+		// eslint-disable-next-line no-mixed-spaces-and-tabs
  		require('tailwindcss')('tailwind.js'),
+		// eslint-disable-next-line no-mixed-spaces-and-tabs
  		require('autoprefixer')()
 	]
 }
